0fa7f351dabd179980e02042e751286a496bf9ba
psychoyit <jbr0085@gmail.com>
2016-03-06 20:02:45 -0600

    first program displaying username

---
+++ project1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void intro();
+
+int main()
+{
+	intro();
+
+	return 0;
+}
+
+void intro()
+{
+	cout << "Bryson's githud ID is : \"psychoyit\"" << endl;
+
+	return;
+}
